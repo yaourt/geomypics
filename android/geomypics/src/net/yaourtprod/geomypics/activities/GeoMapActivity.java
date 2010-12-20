96a14745d1488479bb09edabbe946873e3fa6db9
Yaourt <yaourt@yaourtprod.net>
2010-12-20 18:32:18 +0100

    Add png Marker

--- android/geomypics/src/net/yaourtprod/geomypics/activities/GeoMapActivity.java
+++ android/geomypics/src/net/yaourtprod/geomypics/activities/GeoMapActivity.java
@@ -1,17 +1,27 @@
 package net.yaourtprod.geomypics.activities;
 
 import net.yaourtprod.geomypics.R;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class GeoMapActivity extends MapActivity {
 	/** The Map view.*/
 	private MapView mapView;
 	/** The Map controller.*/
 	private MapController mapController;
+	/** MyLocationOverlay.*/
+	private MyLocationOverlay myLocationOverlay;
+	/** SelectedLocationOverlay.*/
+	private SelectedLocationOverlay selectedLocationOverlay;
 
     /** Called when the activity is first created. */
     @Override
@@ -23,10 +33,30 @@ public class GeoMapActivity extends MapActivity {
         
         mapView.setSatellite(true);
         mapView.setBuiltInZoomControls(true);
+        
+        myLocationOverlay = new MyLocationOverlay(this, mapView);
+//        selectedLocationOverlay = new SelectedLocationOverlay(this, mapView);
     }
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return true;
+	}
+	
+	private static class SelectedLocationOverlay extends Overlay {
+		// http://developer.android.com/resources/tutorials/views/hello-mapview.html
+		// http://mobiforge.com/developing/story/using-google-maps-android
 	}
 }
